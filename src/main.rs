90511de1c000b3b5a10ef92e45b065b2dbe1ccaa
Marcus Bosten <marcus.bosten@googlemail.com>
2023-08-12 18:26:07 +0200

    added clap and fixed clippy recommendations

--- src/main.rs
+++ src/main.rs
@@ -1,13 +1,25 @@
 use chrono::{DateTime, Datelike, Duration, Local};
-use std::{
-    collections::{BTreeSet, HashSet},
-    error::Error,
-    ffi::OsString,
-    fs::{self, DirEntry},
-    path::PathBuf,
-};
+use clap::{command, Parser};
+use std::{collections::BTreeSet, error::Error, ffi::OsString, fs::DirEntry};
+
+#[derive(Parser)] // requires `derive` feature
+#[command(author, version, about, long_about = None)]
+struct Args {
+    #[arg(short = 'e', value_delimiter = ' ', num_args = 1.., default_values = ["tar.gz", "tar.xz"])]
+    endings: Vec<String>,
+
+    #[arg(short = 'f',value_delimiter = ' ', num_args = 1.., default_values = ["/save/backup", "/save2/backup"])]
+    folders: Vec<String>,
+    #[arg(short, long)]
+    dryrun: bool,
+}
 
 fn main() {
+    let args = Args::parse();
+    println!("Endings {:?}!", args.endings);
+    println!("Folders {:?}!", args.folders);
+    println!("Dryrun {:?}!", args.dryrun);
+
     let now = Local::now();
     println!(
         "Current year {}, month {}, day {}",
@@ -15,6 +27,7 @@ fn main() {
         now.month(),
         now.day()
     );
+
     let to = now.year();
 
     // each value once and sorted - dedup for vec needs a sorted vec. Further a set is faster on searching
@@ -32,65 +45,57 @@ fn main() {
     let month_strings = all_months_from_to(from, to);
     to_be_hold.extend(&month_strings);
 
-    let last_1180_days = all_past_180_days(&now);
+    let last_1180_days = past_120_days(&now);
     to_be_hold.extend(&last_1180_days);
 
     // dbg!(&to_be_hold);
+    let endings = args.endings;
+    for folder in args.folders.iter() {
+        let (keep, remove) = create_lists(folder, &to_be_hold, &endings)
+            .expect("could not get lists remove and keep ...");
+        println!("keeeeeep:");
+        println!("{:#?}", keep);
 
-    let endings = vec!["tar.gz".to_owned(), "tar.xz".to_owned()];
-    let (keep, remove) = create_lists("/save2/backup", to_be_hold, endings)
-        .expect("could not get lists remove and keep ...");
-    println!("keeeeeep:");
-    println!("{:#?}", keep);
+        println!("remove:");
+        println!("{:#?}", remove);
 
-    println!("remove:");
-    println!("{:#?}", remove);
+        if !args.dryrun {
+            delete_files(folder, &remove);
+        }
+    }
 }
 
 //create list of files ...
 fn create_lists(
     folder: &str,
-    to_be_hold: BTreeSet<&String>,
-    endings: Vec<String>,
+    to_be_hold: &BTreeSet<&String>,
+    endings: &Vec<String>,
 ) -> Result<(BTreeSet<String>, BTreeSet<String>), OsString> {
     let mut keep = BTreeSet::new();
     let mut remove = BTreeSet::new();
 
-    // let dir = fs::read_dir(folder).unwrap();
-    // let postfix_with_dot = if POSTFIX.starts_with(".") {
-    //     POSTFIX.to_owned()
-    // } else {
-    //     format!(".{}", POSTFIX)
-    // };
-
     for ending in endings {
-        let dir = get_paths_by_ending(folder, &ending);
+        let dir = get_paths_by_ending(folder, ending);
         for entry in dir.expect("path could not be read") {
-            // if let Ok(entry) = entry {
-            // Here, `entry` is a `DirEntry`.
             let file_name = entry.file_name().to_str().unwrap().to_owned();
-            let file_name_lenght_wo_ending =
-                &file_name.chars().count() - &ending.chars().count() - 1;
+
+            let file_name_lenght_wo_ending = file_name.chars().count() - ending.chars().count() - 1;
+
             let file_name_wo_ending = &file_name[0..file_name_lenght_wo_ending].to_owned();
+
             let mut found = false;
             for candidate in to_be_hold.iter() {
-                // let ending_w_postfix = format!("{}{}", ending, postfix_with_dot);
-                // println!(
-                // "found: <{}>, shortened to <{}> check against <{}>",
-                // file_name, file_name_wo_ending, candidate
-                // );
                 if file_name_wo_ending.ends_with(*candidate) {
                     found = true;
-                    // println!("result is: {}", found);
                     break;
                 }
             }
+
             if found {
                 keep.insert(file_name);
             } else {
                 remove.insert(file_name);
             }
-            // }
         }
     }
     Ok((keep, remove))
@@ -100,8 +105,6 @@ fn get_paths_by_ending(dir: &str, ending: &str) -> Result<Vec<DirEntry>, Box<dyn
     let paths = std::fs::read_dir(dir)?
         // Filter out all those directory entries which couldn't be read
         .filter_map(|res| res.ok())
-        // Map the directory entries to paths
-        // .map(|dir_entry| dir_entry.path())
         // Filter out all paths with extensions other than `csv`
         .filter_map(|path| {
             // println!("path in Filtering: {:?}", &path);
@@ -127,8 +130,10 @@ fn get_paths_by_ending(dir: &str, ending: &str) -> Result<Vec<DirEntry>, Box<dyn
     Ok(paths)
 }
 
+fn delete_files(folder: &str, to_be_removed: &BTreeSet<String>) {}
+
 //  a value per year.
-// REMINDER: always to as today, otherwice the value might be lost in quaterly ... .
+// REMINDER: always to as today, otherwice the value might be lost in quaterly, which does not know about 12-01 ... .
 fn all_years_from_to(from: i32, to: i32) -> BTreeSet<String> {
     let mut res = BTreeSet::new();
     let to_plus_one = to + 1;
@@ -165,11 +170,11 @@ fn all_months_from_to(from: i32, to: i32) -> BTreeSet<String> {
     res
 }
 
-// a value each day for the past 180 days and today
-fn all_past_180_days(end_date: &DateTime<Local>) -> BTreeSet<String> {
+// a value each day for the past 120 days and today
+fn past_120_days(end_date: &DateTime<Local>) -> BTreeSet<String> {
     let mut res = BTreeSet::new();
-    for i in 0..180 {
-        let rel_date = end_date.clone() - Duration::days(i);
+    for i in 0..120 {
+        let rel_date = *end_date - Duration::days(i);
         let year = rel_date.year();
         let month = rel_date.month();
         let day = rel_date.day();
